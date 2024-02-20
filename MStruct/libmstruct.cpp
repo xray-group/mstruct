b697319a7de466814862f9b360cd4058a91a255c
Zdenek Matej <zdenek.matej@maxiv.lu.se>
2024-02-20 23:38:07 +0100

    colling MStruct::XMLCrystFileLoadAllObject instead of ObjCryst one

--- MStruct/libmstruct.cpp
+++ MStruct/libmstruct.cpp
@@ -149,7 +149,7 @@ MStruct::ReflectionProfile * _Create_ReflectionProfile(ObjCryst::Crystal* crysta
 
 void _XMLCrystFileLoadAllObject(const std::string& file_name)
 {
-  //MStruct::XMLCrystFileLoadAllObject(file_name);
+  MStruct::XMLCrystFileLoadAllObject(file_name);
 }
 
 MStruct::PowderPattern& _gRefinableObjRegistry_GetPowderpattern(const std::string& name)
@@ -243,7 +243,7 @@ BOOST_PYTHON_MODULE(libMStruct)
   def("test_numpy", test_numpy);
   def("CreateCrystalFromXML", &_XMLLoadCrystal, return_value_policy<manage_new_object>());
   //def("XMLCrystFileLoadAllObject", _XMLCrystFileLoadAllObject);
-  def("XMLCrystFileLoadAllObject", (void (*)(const std::string&)) &ObjCryst::XMLCrystFileLoadAllObject, (bp::arg("name")));
+  def("XMLCrystFileLoadAllObject", (void (*)(const std::string&)) &MStruct::XMLCrystFileLoadAllObject, (bp::arg("name")));
   def("XMLCrystFileSaveGlobal", (void (*)(const std::string&)) &ObjCryst::XMLCrystFileSaveGlobal, (bp::arg("name")));
   def("GetPowderPattern", _gRefinableObjRegistry_GetPowderpattern, return_value_policy<reference_existing_object>());
   def("GetPar", _GetParExtString, return_value_policy<reference_existing_object>());
